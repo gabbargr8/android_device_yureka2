a4eaa968c6dd12f537bb33279178ebbd2a21c4fc
jmjxperia <41988303+jmjxperia@users.noreply.github.com>
2018-11-21 13:28:02 +0530

    Update and rename carbon.mk to carbon_YUREKA2.mk

--- carbon_YUREKA2.mk
+++ carbon_YUREKA2.mk
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+# Inherit Carbon GSM telephony parts
+$(call inherit-product, vendor/carbon/config/gsm.mk)
 
 $(call inherit-product, device/yu/YUREKA2/full_YUREKA2.mk)
 
