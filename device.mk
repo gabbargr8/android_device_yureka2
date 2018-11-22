f23043a40cc228174bf58c5344d3e9e769a62e70
jmjxperia <jmjxperia@users.noreply.github.com>
2018-11-22 21:18:05 +0530

    YUREKA2:implement doze

--- device.mk
+++ device.mk
@@ -408,6 +408,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
-# XiaomiParts
+# Doze
 PRODUCT_PACKAGES += \
-    XiaomiParts
+    YUDoze
